11dc7c958b5667841a588bc3ce1c4ebb2c213742
CarlesRa <jcramos8282@gmail.com>
2021-11-05 00:25:09 +0100

    Ahora si?

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -22,46 +22,47 @@ public class Bird : MonoBehaviour
     {
         _startPosition = _rigidbody2D.position;
         _rigidbody2D.isKinematic = true;
-
-        void OnMouseDown()
-        {
-            _spriteRenderer.color = Color.red;
-        }
-
-        void OnMouseUp()
-        {
-            Vector2 currentPosition = _rigidbody2D.position;
-            Vector2 direction = _startPosition - currentPosition;
-            direction.Normalize();
-
-            _rigidbody2D.isKinematic = false;
-            _rigidbody2D.AddForce(direction * _launchForce);
-
-            _spriteRenderer.color = Color.white;
-        }
-
-        void OnMouseDrag()
-        {
-            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            transform.position = new Vector3(mousePosition.x, mousePosition.y, transform.position.z);
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-
-        void OnCollisionEnter2D(Collision2D collision)
-        {
-            StartCoroutine(ResetAfterDelay());
-        }
-
-        IEnumerator ResetAfterDelay()
-        {
-            yield return new WaitForSeconds(3);
-            _rigidbody2D.position = _startPosition;
-            _rigidbody2D.isKinematic = true;
-            _rigidbody2D.velocity = Vector2.zero;
-        }
     }
+
+    void OnMouseDown()
+    {
+        _spriteRenderer.color = Color.red;
+    }
+
+    void OnMouseUp()
+    {
+        Vector2 currentPosition = _rigidbody2D.position;
+        Vector2 direction = _startPosition - currentPosition;
+        direction.Normalize();
+
+        _rigidbody2D.isKinematic = false;
+        _rigidbody2D.AddForce(direction * _launchForce);
+
+        _spriteRenderer.color = Color.white;
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(mousePosition.x, mousePosition.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        StartCoroutine(ResetAfterDelay());
+    }
+
+    IEnumerator ResetAfterDelay()
+    {
+        yield return new WaitForSeconds(3);
+        _rigidbody2D.position = _startPosition;
+        _rigidbody2D.isKinematic = true;
+        _rigidbody2D.velocity = Vector2.zero;
+    }
+}
